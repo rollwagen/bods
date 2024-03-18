ca9aac1910c9023770489f09710270040031b15b
rollwagen <rollwagen@gmail.com>
2024-03-18 07:22:08 +0100

    WIP

---
+++ pasteboard/pasteboard_darwin.go
@@ -0,0 +1,35 @@
+//go:build darwin
+
+package pasteboard
+
+/*
+#cgo CFLAGS: -x objective-c
+#cgo LDFLAGS: -framework Foundation -framework Cocoa
+#import <Foundation/Foundation.h>
+#import <Cocoa/Cocoa.h>
+
+unsigned int clipboard_read_image(void **out);
+*/
+import "C"
+
+import (
+	"unsafe"
+)
+
+func initialize() error { return nil }
+
+func read_image() (buf []byte, err error) {
+	var (
+		data unsafe.Pointer
+		n    C.uint
+	)
+	n = C.clipboard_read_image(&data)
+	if data == nil {
+		return nil, errUnavailable
+	}
+	defer C.free(data)
+	if n == 0 {
+		return nil, nil
+	}
+	return C.GoBytes(data, C.int(n)), nil
+}
